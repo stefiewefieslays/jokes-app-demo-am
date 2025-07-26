c841326b70994bcd79d07aee1315fe7015a106e2
stefanie <stefanie.chou12@rafflesgirlssch.edu.sg>
2025-07-26 10:09:50 +0800

    jfjvytvhdjfydyt

--- sa-jokes-app/ContentView.swift
+++ sa-jokes-app/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
                  Joke(setup: "Did you hear about the cheese factory that exploded in France?",
                       punchline: "There was nothing left but de Brie"),
                  Joke(setup: "Dad, can you put my shoes on?",
-                      punchline: "I dont think they'll fit me")]
+                    punchline: "I dont think they'll fit me")]
+                Joke(setup: "why did the chicken cross the road", punchline: "to get to the other side of the array")]
     
     @State private var showPunchline = false
     @State private var currentJoke = 0
